cadf6982ee8da4c1cebf36d19f51a8d7602d602b
Jungsub Yoon <cage@live.co.kr>
2020-12-24 17:13:04 +0900

    #4.2 Styling the Movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
   state = {
@@ -8,8 +9,6 @@ class App extends React.Component {
     movies: [],
   };
   getMovies = async () => {
-    // const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
-    // console.log(movies.data.data.movies);
     const {
       data: {
         data: { movies },
@@ -17,8 +16,7 @@ class App extends React.Component {
     } = await axios.get(
       'https://yts-proxy.now.sh/list_movies.json?sort_by=rating'
     );
-    console.log(movies);
-    this.setState({ movies: movies, isLoading: false });
+    this.setState({ movies, isLoading: false });
   };
   componentDidMount() {
     this.getMovies();
@@ -26,23 +24,26 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? 'Loading...'
-          : movies.map((movie) => {
-              // console.log(movie);
-              return (
-                <Movie
-                  key={movie.id}
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                />
-              );
-            })}
-      </div>
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
